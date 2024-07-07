2932918e6435ec8e0e9f9182e5a18807c4bb9c95
Yousef Attia <yousef1907@hotmail.com>
2024-07-07 13:41:03 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -124,10 +124,10 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/build/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/index.html"));
 });
 
 app.listen(port, () => {
