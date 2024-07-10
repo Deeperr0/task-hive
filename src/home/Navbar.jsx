18295fc181a03789e0ace857d96c7d0a074f2a0a
Yousef Attia <yousef1907@hotmail.com>
2024-07-10 11:23:35 +0300

    Improved styling and added option to change password when logged in

--- src/home/Navbar.jsx
+++ src/home/Navbar.jsx
@@ -4,39 +4,54 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-regular-svg-icons";
 import "./Navbar.css";
 import PropTypes from "prop-types";
+import { useNavigate } from "react-router-dom";
 
 export default function Navbar({ loggedIn, username }) {
+	const navigate = useNavigate();
 	const handleLogout = async () => {
 		try {
-			await signOut(auth);
+			await signOut(auth).then(() => {
+				navigate("/");
+			});
 		} catch (error) {
 			console.error("Error logging out:", error);
 		}
 	};
 
-  return (
-    <div className="nav--container">
-      <nav>
-        <div className="nav--left">
-          <h1>TaskHive</h1>
-        </div>
-        <div className="nav--right">
-          <div>
-            {loggedIn ? (
-              <>
-                <button onClick={handleLogout} className="logout">
-                  Logout
-                </button>
-                <h1 className="user-letter">{username}</h1>
-              </>
-            ) : (
-              <FontAwesomeIcon icon={faUser} />
-            )}
-          </div>
-        </div>
-      </nav>
-    </div>
-  );
+	return (
+		<div className="nav--container">
+			<nav>
+				<div className="nav--left">
+					<a href="/">
+						<h1>TaskHive</h1>
+					</a>
+				</div>
+				<div className="nav--right">
+					<div>
+						{loggedIn ? (
+							<>
+								<a
+									href="/change-password"
+									className="change-password"
+								>
+									Change password
+								</a>
+								<button
+									onClick={handleLogout}
+									className="logout"
+								>
+									Logout
+								</button>
+								<h1 className="user-letter">{username}</h1>
+							</>
+						) : (
+							<></>
+						)}
+					</div>
+				</div>
+			</nav>
+		</div>
+	);
 }
 
 Navbar.propTypes = {
