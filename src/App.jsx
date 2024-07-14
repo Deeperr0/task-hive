91cccd22a53722f8f3dc23c4a2cef932ab0021e8
Yousef Attia <yousef1907@hotmail.com>
2024-07-15 00:14:49 +0300

    Added a function to check for username availability

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { auth, db } from "./firebase";
 import { onAuthStateChanged } from "firebase/auth";
-import { doc, getDoc } from "firebase/firestore";
+import { collection, doc, getDoc, getDocs } from "firebase/firestore";
 import Login from "./auth/Login";
 import Home from "./home/Home";
 import Navbar from "./home/Navbar";
@@ -14,105 +14,102 @@ import ChangePassword from "./auth/ChangePassword";
 import "./App.css";
 
 function App() {
-	const [user, setUser] = useState(null);
-	// const [role, setRole] = useState(null);
-	const [loading, setLoading] = useState(true);
-	const [userData, setUserData] = useState({});
-	const [currentWorkSpace, setCurrentWorkSpace] = useState(null);
-	const [expandWorkSpace, setExpandWorkSpace] = useState(false);
-	const [teams, setTeams] = useState([]);
+  const [user, setUser] = useState(null);
+  // const [role, setRole] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [userData, setUserData] = useState({});
+  const [currentWorkSpace, setCurrentWorkSpace] = useState(null);
+  const [expandWorkSpace, setExpandWorkSpace] = useState(false);
+  const [teams, setTeams] = useState([]);
+  const [usersList, setUsersList] = useState([]);
 
-	useEffect(() => {
-		const unsubscribe = onAuthStateChanged(auth, async (currentUser) => {
-			if (currentUser) {
-				console.log("User authenticated:", currentUser);
-				const userDocRef = doc(db, "users", currentUser.uid);
-				const userDoc = await getDoc(userDocRef);
-				if (userDoc.exists()) {
-					const data = userDoc.data();
-					setUserData(data);
-					setTeams(data.teams || []);
-					setCurrentWorkSpace(data.teams[0].teamId);
-				} else {
-					console.log("No such user document!");
-					setUserData(null);
-				}
-			} else {
-				console.log("No user authenticated");
-				setUserData(null);
-			}
-			setUser(currentUser);
-			setLoading(false);
-		});
+  useEffect(() => {
+    async function getUsersList() {
+      const querySnapshot = await getDocs(collection(db, "users"));
+      const users = querySnapshot.docs.map((doc) => doc.data());
+      setUsersList(users);
+    }
+    getUsersList();
+    // Check if user is authenticated
+    const unsubscribe = onAuthStateChanged(auth, async (currentUser) => {
+      if (currentUser) {
+        console.log("User authenticated:", currentUser);
+        const userDocRef = doc(db, "users", currentUser.uid);
+        const userDoc = await getDoc(userDocRef);
+        if (userDoc.exists()) {
+          const data = userDoc.data();
+          setUserData(data);
+          setTeams(data.teams);
+          setCurrentWorkSpace(data.teams[0].teamId);
+        } else {
+          console.log("No such user document!");
+          setUserData(null);
+        }
+      } else {
+        console.log("No user authenticated");
+        setUserData(null);
+      }
+      setUser(currentUser);
+      setLoading(false);
+    });
 
-		return () => unsubscribe();
-	}, []);
+    return () => unsubscribe();
+  }, []);
 
-	if (loading) {
-		return (
-			<div id="loader-container">
-				<Loader />
-			</div>
-		);
-	}
-	return (
-		<Router>
-			<div className="container">
-				<Navbar
-					user={user}
-					userData={userData}
-					setCurrentWorkSpace={setCurrentWorkSpace}
-					currentWorkSpace={currentWorkSpace}
-					setExpandWorkSpace={setExpandWorkSpace}
-					expandWorkSpace={expandWorkSpace}
-					teams={teams}
-				/>
-				<Routes>
-					<Route
-						path="/register"
-						element={<Register setUser={setUser} />}
-					/>
-					<Route
-						path="/reset-password"
-						element={<ResetPassword />}
-					/>
-					{user != null ? (
-						userData.role ? (
-							<Route
-								path="/"
-								element={
-									<Home
-										user={user}
-										userData={userData}
-										role={userData.role}
-										setCurrentWorkSpace={setCurrentWorkSpace}
-										currentWorkSpace={currentWorkSpace}
-										setExpandWorkSpace={setExpandWorkSpace}
-										expandWorkSpace={expandWorkSpace}
-										teams={teams}
-									/>
-								}
-							/>
-						) : (
-							<Route
-								path="/"
-								element={<Loader />}
-							/>
-						)
-					) : (
-						<Route
-							path="/"
-							element={<Login setUser={setUser} />}
-						/>
-					)}
-					<Route
-						path="/change-password"
-						element={<ChangePassword />}
-					/>
-				</Routes>
-			</div>
-		</Router>
-	);
+  if (loading) {
+    return (
+      <div id="loader-container">
+        <Loader />
+      </div>
+    );
+  }
+  console.log("Users list:", usersList);
+  return (
+    <Router>
+      <div className="container">
+        <Navbar
+          user={user}
+          userData={userData}
+          setCurrentWorkSpace={setCurrentWorkSpace}
+          currentWorkSpace={currentWorkSpace}
+          setExpandWorkSpace={setExpandWorkSpace}
+          expandWorkSpace={expandWorkSpace}
+          teams={teams}
+        />
+        <Routes>
+          <Route
+            path="/register"
+            element={<Register setUser={setUser} usersList={usersList} />}
+          />
+          <Route path="/reset-password" element={<ResetPassword />} />
+          {user != null ? (
+            userData.role ? (
+              <Route
+                path="/"
+                element={
+                  <Home
+                    user={user}
+                    userData={userData}
+                    role={userData.role}
+                    setCurrentWorkSpace={setCurrentWorkSpace}
+                    currentWorkSpace={currentWorkSpace}
+                    setExpandWorkSpace={setExpandWorkSpace}
+                    expandWorkSpace={expandWorkSpace}
+                    teams={teams}
+                  />
+                }
+              />
+            ) : (
+              <Route path="/" element={<Loader />} />
+            )
+          ) : (
+            <Route path="/" element={<Login setUser={setUser} />} />
+          )}
+          <Route path="/change-password" element={<ChangePassword />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -7,117 +7,111 @@ import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 import PropTypes from "prop-types";
 
 export default function Login({ setUser }) {
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
-	const [showPassword, setShowPassword] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
 
-	async function handleLogin(event) {
-		event.preventDefault();
-		try {
-			const userCredential = await signInWithEmailAndPassword(
-				auth,
-				email,
-				password
-			);
-			setTimeout(() => {
-				setUser(userCredential.user);
-			}, 2000);
-		} catch (error) {
-			const loginError = document.querySelector(".login-error");
-			loginError.classList.remove("hidden");
-			console.log(error);
-			switch (error.message) {
-				case "Firebase: Error (auth/user-disabled).":
-					loginError.innerHTML = "User disabled. Please contact support.";
-					break;
-				case "Firebase: Error (auth/user-not-found).":
-					loginError.innerHTML = "User not found. Please register first.";
-					break;
-				case "Firebase: Error (auth/wrong-password).":
-					loginError.innerHTML = "Wrong password. Please try again.";
-					break;
-				case "Firebase: Error (auth/too-many-requests).":
-					loginError.innerHTML = "Too many requests. Please try again later.";
-					break;
-				case "Firebase: Error (auth/invalid-email).":
-					loginError.innerHTML = "Invalid email. Please try again.";
-					break;
-				case "Firebase: Error (auth/network-request-failed).":
-					loginError.innerHTML = "Network request failed. Please try again.";
-					break;
-				case "Firebase: Error (auth/weak-password).":
-					loginError.innerHTML = "Password must be at least 6 characters.";
-					break;
-				case "Firebase: Error (auth/invalid-credential).":
-					loginError.innerHTML = "Invalid credentials. Please try again.";
-					break;
-				default:
-					loginError.innerHTML = "An error occurred. Please try again.";
-					break;
-			}
-			console.error("Error logging in:", error);
-		}
-	}
+  async function handleLogin(event) {
+    event.preventDefault();
+    try {
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      setTimeout(() => {
+        setUser(userCredential.user);
+      }, 2000);
+    } catch (error) {
+      const loginError = document.querySelector(".login-error");
+      loginError.classList.remove("hidden");
+      console.log(error);
+      switch (error.message) {
+        case "Firebase: Error (auth/user-disabled).":
+          loginError.innerHTML = "User disabled. Please contact support.";
+          break;
+        case "Firebase: Error (auth/user-not-found).":
+          loginError.innerHTML = "User not found. Please register first.";
+          break;
+        case "Firebase: Error (auth/wrong-password).":
+          loginError.innerHTML = "Wrong password. Please try again.";
+          break;
+        case "Firebase: Error (auth/too-many-requests).":
+          loginError.innerHTML = "Too many requests. Please try again later.";
+          break;
+        case "Firebase: Error (auth/invalid-email).":
+          loginError.innerHTML = "Invalid email. Please try again.";
+          break;
+        case "Firebase: Error (auth/network-request-failed).":
+          loginError.innerHTML = "Network request failed. Please try again.";
+          break;
+        case "Firebase: Error (auth/weak-password).":
+          loginError.innerHTML = "Password must be at least 6 characters.";
+          break;
+        case "Firebase: Error (auth/invalid-credential).":
+          loginError.innerHTML = "Invalid credentials. Please try again.";
+          break;
+        default:
+          loginError.innerHTML = "An error occurred. Please try again.";
+          break;
+      }
+      console.error("Error logging in:", error);
+    }
+  }
 
-	return (
-		<div className="login-container">
-			<h2 className="login-title">Login</h2>
-			<form
-				onSubmit={handleLogin}
-				className="login-form"
-			>
-				<input
-					type="email"
-					value={email}
-					onChange={(e) => setEmail(e.target.value)}
-					placeholder="Email"
-					required
-				/>
-				<div className="password-field">
-					<input
-						type={showPassword ? "text" : "password"}
-						value={password}
-						onChange={(e) => setPassword(e.target.value)}
-						placeholder="Password"
-						required
-						minLength={6}
-						maxLength={20}
-					/>
-					<button
-						type="button"
-						className="show-password"
-						onClick={(e) => {
-							e.preventDefault();
-							setShowPassword(!showPassword);
-						}}
-					>
-						{showPassword ? (
-							<FontAwesomeIcon icon={faEye} />
-						) : (
-							<FontAwesomeIcon icon={faEyeSlash} />
-						)}
-					</button>
-				</div>
-				<button type="submit">Login</button>
-				<p className="reset-password">
-					{`Forgot your password? `}
-					<a
-						href="/reset-password"
-						className="reset-password-link"
-					>
-						Reset password
-					</a>
-				</p>
-			</form>
-			<p className="no-account">
-				{"Don't have an account?"} <a href="/signup">Sign up</a>
-			</p>
+  return (
+    <div className="login-container">
+      <h2 className="login-title">Login</h2>
+      <form onSubmit={handleLogin} className="login-form">
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+          required
+        />
+        <div className="password-field">
+          <input
+            type={showPassword ? "text" : "password"}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+            required
+            minLength={6}
+            maxLength={20}
+          />
+          <button
+            type="button"
+            className="show-password"
+            onClick={(e) => {
+              e.preventDefault();
+              setShowPassword(!showPassword);
+            }}
+          >
+            {showPassword ? (
+              <FontAwesomeIcon icon={faEye} />
+            ) : (
+              <FontAwesomeIcon icon={faEyeSlash} />
+            )}
+          </button>
+        </div>
+        <button type="submit">Login</button>
+        <p className="reset-password">
+          {`Forgot your password? `}
+          <a href="/reset-password" className="reset-password-link">
+            Reset password
+          </a>
+        </p>
+      </form>
+      <p className="no-account">
+        {"Don't have an account?"} <a href="/register">Sign up</a>
+      </p>
 
-			<p className="login-error hidden">{`Error: ${auth.error}`}</p>
-		</div>
-	);
+      <p className="login-error hidden">{`Error: ${auth.error}`}</p>
+    </div>
+  );
 }
 
 Login.propTypes = {
-	setUser: PropTypes.func,
+  setUser: PropTypes.func,
 };
