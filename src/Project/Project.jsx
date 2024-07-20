cf610063762e04c800795034b164b24b8887f8c9
Yousef Attia <yousef1907@hotmail.com>
2024-07-20 16:49:20 +0200

    Removed unnecessary files

---
+++ src/Project/Project.jsx
@@ -0,0 +1,219 @@
+import AddUser from "../components/AddUser";
+import Filters from "../components/Filters";
+import { useContext, useState } from "react";
+import { updateDoc, doc } from "firebase/firestore";
+import { db } from "../firebase";
+import Tasks from "../components/Tasks";
+import "./Project.css";
+import PropTypes from "prop-types";
+import { WorkSpaceContext } from "../App";
+import useFilterTasks from "../hooks/useFilterTasks";
+import getUserDoc from "../utils/getUserDoc";
+
+export default function Project({ user, userData, usersList }) {
+	const { currentWorkSpace, setCurrentWorkSpace } =
+		useContext(WorkSpaceContext);
+	const [priorityFilter, setPriorityFilter] = useState("");
+	const [statusFilter, setStatusFilter] = useState("");
+	const [toggleAddUser, setToggleAddUser] = useState(false);
+
+	const filteredTasks = useFilterTasks(
+		priorityFilter,
+		statusFilter,
+		currentWorkSpace
+	);
+
+	async function addTask() {
+		if (currentWorkSpace.role !== "admin") return; // Ensure only admin can add tasks
+
+		const newTask = {
+			taskId: `task-${Date.now()}`, // Generate a unique task ID
+			content: "New Task",
+			owner: userData.username,
+			ownerUid: user.uid,
+			status: "Not started",
+			deadline: new Date().toISOString().split("T")[0],
+			priority: "Low",
+			notes: "",
+			lastUpdated: new Date().toISOString(),
+		};
+
+		try {
+			await Promise.all(
+				usersList.map(async (userItem) => {
+					if (
+						userItem.teams.some(
+							(team) => team.teamId === currentWorkSpace.teamId
+						)
+					) {
+						const userDoc = await getUserDoc(userItem.username);
+
+						if (!userDoc) {
+							return;
+						}
+						const userData = userDoc.data();
+						const updatedTeams = userData.teams.map((team) => {
+							if (team.teamId === currentWorkSpace.teamId) {
+								return {
+									...team,
+									tasks: [...team.tasks, newTask],
+								};
+							}
+							return team;
+						});
+
+						const userDocRef = doc(db, "users", userDoc.id);
+						await updateDoc(userDocRef, { teams: updatedTeams });
+						setCurrentWorkSpace(
+							updatedTeams.filter(
+								(team) => team.teamId === currentWorkSpace.teamId
+							)[0]
+						);
+					}
+				})
+			);
+		} catch (error) {
+			console.error("Error adding task:", error);
+		}
+	}
+	async function updateTask(taskId, updates) {
+		try {
+			await Promise.all(
+				usersList.map(async (userItem) => {
+					if (
+						userItem.teams.some(
+							(team) => team.teamId === currentWorkSpace.teamId
+						)
+					) {
+						const userDoc = await getUserDoc(userItem.username);
+						if (!userDoc) {
+							return;
+						}
+						const userData = userDoc.data();
+						const updatedTeams = userData.teams.map((team) => {
+							if (team.teamId === currentWorkSpace.teamId) {
+								const updatedTasks = team.tasks.map((task) => {
+									if (task.taskId === taskId) {
+										return { ...task, ...updates };
+									}
+									return task;
+								});
+								setCurrentWorkSpace({
+									...currentWorkSpace,
+									tasks: updatedTasks,
+								});
+								return { ...team, tasks: updatedTasks };
+							}
+							return team;
+						});
+
+						const userDocRef = doc(db, "users", userDoc.id);
+						await updateDoc(userDocRef, { teams: updatedTeams });
+					}
+				})
+			);
+		} catch (error) {
+			console.error("Error updating task:", error);
+		}
+	}
+
+	async function deleteTask(taskId) {
+		try {
+			await Promise.all(
+				usersList.map(async (userItem) => {
+					if (
+						userItem.teams.some(
+							(team) => team.teamId === currentWorkSpace.teamId
+						)
+					) {
+						const userDoc = await getUserDoc(userItem.username);
+						if (!userDoc) {
+							return;
+						}
+						const userData = userDoc.data();
+						const updatedTeams = userData.teams.map((team) => {
+							if (team.teamId === currentWorkSpace.teamId) {
+								const updatedTasks = team.tasks.filter(
+									(task) => task.taskId !== taskId
+								);
+								return { ...team, tasks: updatedTasks };
+							}
+							return team;
+						});
+
+						const userDocRef = doc(db, "users", userDoc.id);
+						await updateDoc(userDocRef, { teams: updatedTeams });
+						setCurrentWorkSpace(
+							updatedTeams.filter(
+								(team) => team.teamId === currentWorkSpace.teamId
+							)[0]
+						);
+					}
+				})
+			);
+		} catch (error) {
+			console.error("Error deleting task:", error);
+		}
+	}
+
+	return (
+		<div className="project">
+			{toggleAddUser && (
+				<AddUser
+					toggleAddUser={toggleAddUser}
+					setToggleAddUser={setToggleAddUser}
+					user={user}
+					currentWorkSpace={currentWorkSpace}
+				/>
+			)}
+
+			<h2 className="project-title">Project Tasks</h2>
+			{currentWorkSpace.role === "admin" && (
+				<div className="add-buttons">
+					<button
+						onClick={addTask}
+						className="add-button"
+					>
+						New task
+					</button>
+					<button
+						className="add-user"
+						onClick={() => setToggleAddUser(true)}
+					>
+						Add user
+					</button>
+				</div>
+			)}
+			<Filters
+				setPriorityFilter={setPriorityFilter}
+				setStatusFilter={setStatusFilter}
+			/>
+			<Tasks
+				name="To do"
+				tasksList={
+					currentWorkSpace.role === "admin"
+						? filteredTasks.filter((task) => task.status != "Done")
+						: filteredTasks
+								.filter((task) => task.status != "Done")
+								.filter((task) => task.ownerUid === user.uid)
+				}
+				updateTask={updateTask} // Pass updateTask function
+				deleteTask={deleteTask}
+			/>
+			<Tasks
+				name="Done"
+				tasksList={currentWorkSpace.tasks
+					.filter((task) => task.status === "Done")
+					.filter((task) => task.ownerUid === user.uid)}
+				updateTask={updateTask} // Pass updateTask function
+				deleteTask={deleteTask}
+			/>
+		</div>
+	);
+}
+
+Project.propTypes = {
+	user: PropTypes.object.isRequired,
+	userData: PropTypes.object.isRequired,
+	usersList: PropTypes.array.isRequired,
+};
